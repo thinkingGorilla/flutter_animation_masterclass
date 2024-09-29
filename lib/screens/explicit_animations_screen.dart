f4608a58c3b66803af09f89374229b6fb5fa8c54
thinkingGorilla <4zxzxz4@gmail.com>
2024-09-29 11:42:04 +0900

    Explicit animation 구현 두번째
    - AnimationController 주요 함수 학습

--- lib/screens/explicit_animations_screen.dart
+++ lib/screens/explicit_animations_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ExplicitAnimationsScreen extends StatefulWidget {
@@ -7,26 +9,52 @@ class ExplicitAnimationsScreen extends StatefulWidget {
   State<ExplicitAnimationsScreen> createState() => _ExplicitAnimationsScreenState();
 }
 
-// `SingleTickerProviderStateMixin`는 현재 화면이 트리에 있을 때만 사용가능한 `Ticker`를 제공한다.
 class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen> with SingleTickerProviderStateMixin {
-  late final AnimationController _animationController = AnimationController(
-    // 애니메이션이 매 프레임마다 실행되어야하므로
-    // `SingleTickerProviderStateMixin`로부터 제공받은 `Ticker`를 애니메이션을 제어하는 컨트롤러에 동기화한다.
-    vsync: this,
-  );
+  late final AnimationController _animationController;
 
   @override
   void initState() {
     super.initState();
-    // `Ticker`는 매 프레임마다 콜백 함수를 호출한다.
-    // `Ticker`를 생성한 화면이 트리에서 사라져도 `Ticker`는 계속 동작한다.
-    // Ticker((elapsed) => print(elapsed)).start();
+    _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 10));
+
+    Timer.periodic(const Duration(milliseconds: 500), (timer) => super.setState(() {}));
+  }
+
+  void _play() => _animationController.forward();
+
+  void _pause() => _animationController.stop();
+
+  void _rewind() => _animationController.reverse();
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Explicit Animations')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '${_animationController.value}',
+              style: const TextStyle(fontSize: 28),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: _play, child: const Text('Play')),
+                ElevatedButton(onPressed: _pause, child: const Text('Pause')),
+                ElevatedButton(onPressed: _rewind, child: const Text('Rewind')),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
