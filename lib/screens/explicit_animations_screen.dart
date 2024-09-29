b40fa777850285b6df4bbe8f7c76605ed28c6d27
thinkingGorilla <4zxzxz4@gmail.com>
2024-09-29 12:26:00 +0900

    Explicit animation 구현 네번째
    - AnimationController.value을 Tween에 매핑하여 애니메이션 목표값을 구하는 방법 학습

--- lib/screens/explicit_animations_screen.dart
+++ lib/screens/explicit_animations_screen.dart
@@ -9,18 +9,17 @@ class ExplicitAnimationsScreen extends StatefulWidget {
 
 class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen> with SingleTickerProviderStateMixin {
   late final AnimationController _animationController;
+  late final Animation<Color?> _color;
 
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 10));
-    // `AnimationController`의 addListener() 함수를 통해 매 프레임마다 setState()를 호출하여 화면을 리렌더링한다.
-    // 하지만 매 프레임마다 화면 전체에 대해 setState()를 호출하는 것은 매우 비효율적이다.
-    // ..addListener(() => setState(() {}));
-
-    // `Timer`를 이용하여 setState()를 호출하여 화면을 리렌더링한다.
-    // 하지만 다시 리렌더링하기 위해 `periodic` 동안 기다려야하는 문제점이 있다.
-    // Timer.periodic(const Duration(milliseconds: 500), (_) => super.setState(() {}));
+    _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    // `Tween`이 `AnimationController`에 의해 애니메이션되도록
+    // animate() 함수를 통해 `AnimationController`와 연결한다.
+    // `Tween`은 `AnimationController`에서 제공하는 0부터 1까지의 실수 값을 따라
+    // 설정된 `begin`부터 `end`까지의 값으로부터 자신이 애니메이션할 목표값을 보간하여 계산한다.
+    _color = ColorTween(begin: Colors.amber, end: Colors.red).animate(_animationController);
   }
 
   void _play() => _animationController.forward();
@@ -43,27 +42,17 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen> wit
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // 애니메이션에 의해 리렌더링되는 영역을 AnimatedBuilder 위젯 안의 영역으로 제한한다.
             AnimatedBuilder(
-              animation: _animationController,
+              animation: _color,
               builder: (context, child) {
-                return Opacity(
-                  // `AnimationController`의 value 값은 0부터 1까지 범위의 값을 가지는데
-                  // 이 값들만으로는 다양한 애니메이션을 만들 수 없다.
-                  // 이를 해결하기 위해 `Tween`을 사용한다.
-                  opacity: _animationController.value,
-                  child: Container(
-                    color: Colors.amber,
-                    width: 400,
-                    height: 400,
-                  ),
+                return Container(
+                  // `AnimationController.value`에 의해 `ColorTween.value`에 매핑된 값을 사용한다.
+                  color: _color.value,
+                  width: 400,
+                  height: 400,
                 );
               },
             ),
-            // Text(
-            //   '${_animationController.value}',
-            //   style: const TextStyle(fontSize: 28),
-            // ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
