2e5909f203562aac3eb4fc21e6ee53eb7fe7c7a1
thinkingGorilla <4zxzxz4@gmail.com>
2024-09-29 12:11:30 +0900

    Explicit animation 구현 세번째
    - 화면을 리렌더링하기 위해 상태 변경을 다른 위젯에 위임하는 이유와 AnimationController.value 값의 한계 및 Tween의 필요성 학습

--- lib/screens/explicit_animations_screen.dart
+++ lib/screens/explicit_animations_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 class ExplicitAnimationsScreen extends StatefulWidget {
@@ -16,8 +14,13 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen> wit
   void initState() {
     super.initState();
     _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 10));
+    // `AnimationController`의 addListener() 함수를 통해 매 프레임마다 setState()를 호출하여 화면을 리렌더링한다.
+    // 하지만 매 프레임마다 화면 전체에 대해 setState()를 호출하는 것은 매우 비효율적이다.
+    // ..addListener(() => setState(() {}));
 
-    Timer.periodic(const Duration(milliseconds: 500), (timer) => super.setState(() {}));
+    // `Timer`를 이용하여 setState()를 호출하여 화면을 리렌더링한다.
+    // 하지만 다시 리렌더링하기 위해 `periodic` 동안 기다려야하는 문제점이 있다.
+    // Timer.periodic(const Duration(milliseconds: 500), (_) => super.setState(() {}));
   }
 
   void _play() => _animationController.forward();
@@ -40,10 +43,27 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen> wit
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              '${_animationController.value}',
-              style: const TextStyle(fontSize: 28),
+            // 애니메이션에 의해 리렌더링되는 영역을 AnimatedBuilder 위젯 안의 영역으로 제한한다.
+            AnimatedBuilder(
+              animation: _animationController,
+              builder: (context, child) {
+                return Opacity(
+                  // `AnimationController`의 value 값은 0부터 1까지 범위의 값을 가지는데
+                  // 이 값들만으로는 다양한 애니메이션을 만들 수 없다.
+                  // 이를 해결하기 위해 `Tween`을 사용한다.
+                  opacity: _animationController.value,
+                  child: Container(
+                    color: Colors.amber,
+                    width: 400,
+                    height: 400,
+                  ),
+                );
+              },
             ),
+            // Text(
+            //   '${_animationController.value}',
+            //   style: const TextStyle(fontSize: 28),
+            // ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
