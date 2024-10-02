03ada53187bd6345a95f6597fe68e655e24f5a30
thinkingGorilla <4zxzxz4@gmail.com>
2024-10-02 16:18:37 +0900

    SwipingCards 구현 두번째
    - Tween을 사용하여 X축 변화 시 카드가 회전하도록 구현하는 방법 학습

--- lib/screens/swiping_cards_screen.dart
+++ lib/screens/swiping_cards_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SwipingCardsScreen extends StatefulWidget {
@@ -15,6 +17,8 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen> with SingleTick
     duration: const Duration(milliseconds: 300),
     value: 0,
   );
+
+  late final Tween<double> _rotation = Tween(begin: -15.0, end: 15.0);
 
   void _onHorizontalDragUpdate(DragUpdateDetails details) => _position.value += details.delta.dx;
 
@@ -35,6 +39,16 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen> with SingleTick
       body: AnimatedBuilder(
         animation: _position,
         builder: (context, child) {
+          // `transform`은 0부터 1사이의 값을 사용하여 `begin`부터 `end`사이의 값을 보간하여 구한다.
+          // 따라서 -15도부터 +15도 사이의 각도 값을 구하기 위해서는
+          // 먼저 `_position` 값을 0부터 1사이의 값으로 변환해야한다.
+          // [-width, 0, +width]를 `width`로 나눈다.
+          // [-1, 0, +1]에 1을 더한다.
+          // [0, 1, 2]를 2로 나눈다.
+          // [0, 0, 1] = 0부터 1
+          // 결론적으로 `position`를 `width`로 나누고 1을 더하고 2로 나누면 0부터 1사이의 값으로 변환할 수 있다.
+          final angle = _rotation.transform((_position.value / _size.width + 1) / 2);
+          // print(angle);
           return Stack(
             children: [
               Align(
@@ -44,12 +58,16 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen> with SingleTick
                   onHorizontalDragEnd: _onHorizontalDragEnd,
                   child: Transform.translate(
                     offset: Offset(_position.value, 0),
-                    child: Material(
-                      elevation: 10,
-                      color: Colors.red.shade100,
-                      child: SizedBox(
-                        width: _size.width * .8,
-                        height: _size.height * .5,
+                    child: Transform.rotate(
+                      // 각도를 호도법으로 변환한다.(2*pi[*rad] / 360도)
+                      angle: angle * pi / 180,
+                      child: Material(
+                        elevation: 10,
+                        color: Colors.red.shade100,
+                        child: SizedBox(
+                          width: _size.width * .8,
+                          height: _size.height * .5,
+                        ),
                       ),
                     ),
                   ),
